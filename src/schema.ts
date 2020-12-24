6efc3e9638c86d4b71433cb49572d78071aa2087
metleeha <nodongth@gmail.com>
2020-12-24 16:13:43 +0900

    Feature: Datamodel with Prisma2

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,9 @@ import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 import path from "path";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const allTypes: any[] = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
 const allResolvers: any[] = fileLoader(path.join(__dirname, "/api/**/*.ts"));
