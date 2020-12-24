f8c2f1d4bf767963a123c50e01b5472d60e70e54
metleeha <nodongth@gmail.com>
2020-12-24 16:19:27 +0900

    Test: Prisma2

--- src/api/Greetings/sayHello/sayHello.ts
+++ src/api/Greetings/sayHello/sayHello.ts
@@ -1,5 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export default {
     Query: {
-        sayHello: () => "Hello"
+        sayHello: async () => {
+            console.log(await prisma.user.findMany());
+            return "HELLO"
+        }
     }
 }
